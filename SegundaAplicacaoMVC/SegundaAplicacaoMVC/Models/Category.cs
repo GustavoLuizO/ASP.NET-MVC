aeee257e7e3e0a05571ff7392fc1d186ffc17f2d
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-16 17:01:02 -0300

    Colocando nome nos titulos do html

--- SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
+++ SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
@@ -1,8 +1,16 @@
-﻿namespace SegundaAplicacaoMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SegundaAplicacaoMVC.Models
 {
     public class Category
     {
+        [Display(Name = "Category")]
         public long idCategory { get; set; }
+        [Display(Name = "Name of Category")]
         public string name { get; set; }
     }
 }
