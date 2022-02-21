2dae6f5f44b6e8c4c07d916781a0b50a771dff81
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-21 13:26:45 -0300

    Create View
    Criação da view para poder ver o html no site

--- SegundaAplicacaoMVC/SegundaAplicacaoMVC/Controllers/CategoryController.cs
+++ SegundaAplicacaoMVC/SegundaAplicacaoMVC/Controllers/CategoryController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SegundaAplicacaoMVC.Models;
+using System.Collections.Generic;
 
 namespace SegundaAplicacaoMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        public static IList<Category> categorylist = new List<Category>()
+        {
+            new Category()
+            {
+                CategoryID = 1,
+                Name = "Eletrodomésticos"
+            },
+            new Category()
+            {
+                CategoryID = 2,
+                Name = "Celulares"
+            },
+            new Category()
+            {
+                CategoryID = 3,
+                Name = "Notebooks"
+            },
+            new Category()
+            {
+                CategoryID = 4,
+                Name = "Instrumentos musicais"
+            },
+            new Category()
+            {
+                CategoryID = 5,
+                Name = "Materiais Esportivos"
+            }
+        };
+
         public IActionResult Index()
         {
-            return View();
+            return View(categorylist);
         }
     }
 }
--- SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
+++ SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace SegundaAplicacaoMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace SegundaAplicacaoMVC.Models
 {
     public class Category
     {
+        [Display(Name = "Category Code")]
         public long CategoryID { get; set; }
+
+        [Display(Name = "Category Name")]
         public string Name { get; set; }
     }
 }
