389e7e75faddbd01937648ddb03f3f1bfa40fcf6
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-21 13:13:23 -0300

    Criação da NovaSegundaAplicacaoMVC
    Criação da nova segunda aplicação MVC.
    Trocada para está versão pois possui o startup que não tinha a outra.

--- SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
+++ SegundaAplicacaoMVC/SegundaAplicacaoMVC/Models/Category.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-
-namespace SegundaAplicacaoMVC.Models
+﻿namespace SegundaAplicacaoMVC.Models
 {
     public class Category
     {
-        [Display(Name = "Category")]
-        public long idCategory { get; set; }
-        [Display(Name = "Name of Category")]
-        public string name { get; set; }
+        public long CategoryID { get; set; }
+        public string Name { get; set; }
     }
 }
