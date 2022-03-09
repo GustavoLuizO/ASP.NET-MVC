2f8b8c28a1e3f62643efb1a19f8861b898f0c43f
Gustavo Luiz <glogustavo4321@gmail.com>
2022-03-09 20:06:03 -0300

    Create manufacturer.cs
    Criação da classe fabricante

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/manufacturer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+namespace APP_with_ENitty_Framework.Models
+{
+    public class manufacturer
+    {
+        public int manufacturerId { get; set; }
+        public string name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
