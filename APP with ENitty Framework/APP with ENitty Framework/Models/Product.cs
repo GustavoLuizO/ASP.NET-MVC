9296daf652e3098aa72d9b24e076c8abc4b9aa8f
Gustavo Luiz <glogustavo4321@gmail.com>
2022-03-09 20:06:15 -0300

    Create Product.cs
    Criação da classs product

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace APP_with_ENitty_Framework.Models
+{
+    public class Product
+    {
+        public int produutoId { get; set; }
+        public string name { get; set; }
+        public float cost { get; set; }
+        public int manufacturerId { get; set; }
+        public manufacturer manufacturer { get; set; }
+    }
+}
