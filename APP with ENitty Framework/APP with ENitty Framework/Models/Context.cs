c8425fbdf28ea86933107893b858aca7ad6caa65
Gustavo Luiz <glogustavo4321@gmail.com>
2022-03-09 20:11:34 -0300

    Create Context.cs
    Create classe Context

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APP_with_ENitty_Framework.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<manufacturer> manafactures { get; set; }
+    }
+}
