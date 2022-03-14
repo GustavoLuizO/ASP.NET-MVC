d2591cc7892c168b350880eee3a5db359b97595c
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-13 23:35:52 -0300

    Create Fabricante.cs

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/Fabricante.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace APP_with_ENitty_Framework.Models
+{
+    public class Fabricante
+    {
+        public int FabricanteId { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
