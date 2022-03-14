55b0ab6fe4e71b29e00c7eca0ed5358e9db3ea09
Gustavo Luiz de Oliveira <75046198+GustavoLuizO@users.noreply.github.com>
2022-03-13 22:55:33 -0300

    Criação

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APP_with_ENitty_Framework.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
