57f612c695dc5ad9d8a66e69d7e5bf6e3bc7dd35
Gustavo Luiz <glogustavo4321@gmail.com>
2022-03-14 21:25:56 -0300

    Create SeedData.cs

---
+++ APP with ENitty Framework/APP with ENitty Framework/Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace APP_with_ENitty_Framework.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            //associa os dados ao contexto
+            Context context = app.ApplicationServices.GetRequiredService<Context>();
+            //inserir os dados nas entidades do contexto
+            context.Database.Migrate();
+            //se o contexto estiver vazio
+            
+            if(!context.Produtos.Any())
+            //inserir os produtos inicias
+            {
+                context.Produtos.AddRange(
+                    new Produto { Nome = "Camisa Social", preco = 250, FabricanteId = 1 },
+                    new Produto { Nome = "Short", preco = 120, FabricanteId = 1 },
+                    new Produto { Nome = "Tenis", preco = 540, FabricanteId = 2 });
+                context.Fabricantes.AddRange(
+                    new Fabricante { Nome = "Adidas" },
+                    new Fabricante { Nome = "Nike" });
+            }
+        }
+    }
+}
