a5a064bceaca0d077b80b021b155621caaa87aa9
Gustavo Luiz <glogustavo4321@gmail.com>
2022-04-04 21:57:45 -0300

    Create ClienteController.cs
    Criação do cliente controller

---
+++ AppWorkWithDataAnnotations/AppWorkWithDataAnnotations/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using AppWorkWithDataAnnotations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWorkWithDataAnnotations.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Cliente cliente)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    return RedirectToAction("Create"); 
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
